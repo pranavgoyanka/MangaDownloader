a2ed43a2fd3cf0eebb47c2ff76699de94e59bd66
pranavgoyanka <pgoyanka@gmail.com>
2018-11-13 21:28:31 +0530

    Automatically Download All chapters from a giver range of chapters

--- manga.py
+++ manga.py
@@ -11,30 +11,47 @@ page = 1
 # k = input("Press any key to proceed...")
     
 baseAdd = input("Enter the base address of your manga from MangaPand\neg: https://www.mangapanda.com/shingeki-no-kyojin/\nBase Address: ")
+inich = int(input("Enter Initial Chapter Number: "))
+finch = int(input("Enter Final Chapter Number: "))
+# fin = int(input("Enter last page number of chapter: "))
+# fin = int(soup.find_all("option")[-1].get_text())
+fin = 2;
+ch = inich
+for ch in range(ch,  finch+1):
+    if os.path.isdir(baseAdd[26:]) == False:
+        os.mkdir(baseAdd[26:])    
 
-baseAdd = "https://www.mangapanda.com/shingeki-no-kyojin/"
-ch = int(input("Enter Chapter Number: "))
-end = int(input("Enter last page number of chapter: "))
+    os.chdir(baseAdd[26:])
+    if os.path.isdir(str(ch)) == False:
+        os.mkdir(str(ch))    
 
-# for ch in range(1,  112):
-os.mkdir(str(ch))
-os.chdir(str(ch))
-for i in range(1, end + 1):
-    url = baseAdd + str(ch) + "/" + str(page)
+    os.chdir(str(ch))
+
+    url = baseAdd + str(ch)
     r = requests.get(url)
     r.content
     soup = BeautifulSoup(r.content);
-    imgurl = soup.find_all("img", {"id":"img"})
-    imgurls = str(imgurl)
-    start = imgurls.find('src=\"') + 5
-    end = imgurls.find('.jpg') + 4
-    imgurls = imgurls[start:end]
-    print(imgurls)
-    class AppURLopener(urllib.request.FancyURLopener):
-        version = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.69 Safari/537.36"
-    urllib._urlopener = AppURLopener()
-    urllib._urlopener.retrieve(imgurls, str(page) + ".jpg")
-    page = page + 1
-    # os.chdir('..')
+    time.sleep(2)
+    fin = int(soup.find_all("option")[-1].get_text())
+
+    for i in range(1, fin + 1):
+        url = baseAdd + str(ch) + "/" + str(page)
+        r = requests.get(url)
+        r.content
+        soup = BeautifulSoup(r.content);
+        # fin = int(soup.find_all("option")[-1].get_text())
+        imgurl = soup.find_all("img", {"id":"img"})
+        imgurls = str(imgurl)
+        # lastpg = soup.find_all("div", {"id":"selectpage"})
+        start = imgurls.find('src=\"') + 5
+        end = imgurls.find('.jpg') + 4
+        imgurls = imgurls[start:end]
+        print(imgurls)
+        class AppURLopener(urllib.request.FancyURLopener):
+            version = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.69 Safari/537.36"
+        urllib._urlopener = AppURLopener()
+        urllib._urlopener.retrieve(imgurls, str(page) + ".jpg")
+        page = page + 1
+    os.chdir('..')
 
 k = input("Press any key to exit...")

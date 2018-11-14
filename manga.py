6705c0d8ded3740e9e3b94e9de97b67d27d46a8e
pranavgoyanka <pgoyanka@gmail.com>
2018-11-14 23:27:29 +0530

    Changed some directory related stuff

--- manga.py
+++ manga.py
@@ -5,47 +5,53 @@ import urllib.request
 from bs4 import BeautifulSoup
 import os
 from tqdm import tqdm
+import os.path
 ch = 1 
 page = 1
-    
+print(os.getcwd())
 baseAdd = input("Enter the base address of your manga from MangaPand\neg: https://www.mangapanda.com/shingeki-no-kyojin/\nBase Address: ")
 inich = int(input("Enter Initial Chapter Number: "))
 finch = int(input("Enter Final Chapter Number: "))
-
+skip = input("Overwrite existing files? (y/n):")
+print(skip)
 fin = 2;
 ch = inich
-for ch in range(ch,  finch+1):
-    print('\n')
-    if os.path.isdir(baseAdd[26:]) == False:
-        os.mkdir(baseAdd[26:])    
+if os.path.isdir("." + baseAdd[26:]) == False:
+    os.mkdir("."+baseAdd[26:])    
 
-    os.chdir(baseAdd[26:])
-    if os.path.isdir(str(ch)) == False:
-        os.mkdir(str(ch))    
+os.chdir("." + baseAdd[26:])
 
-    os.chdir(str(ch))
+for ch in range(inich,  finch+1):
+
+    if os.path.isdir("./" + str(ch)) == False:
+        os.mkdir("./"+str(ch))    
+
+    os.chdir("./"+str(ch))
 
     url = baseAdd + str(ch)
+    url
     r = requests.get(url)
     r.content
     soup = BeautifulSoup(r.content);
     fin = int(soup.find_all("option")[-1].get_text())
 
-    for i in tqdm(range(1, fin + 1), desc="Chapter " + str(ch), ascii=True):
-        url = baseAdd + str(ch) + "/" + str(page)
-        r = requests.get(url)
-        r.content
-        soup = BeautifulSoup(r.content);
-        imgurl = soup.find_all("img", {"id":"img"})
-        imgurls = str(imgurl)
-        start = imgurls.find('src=\"') + 5
-        end = imgurls.find('.jpg') + 4
-        imgurls = imgurls[start:end]
-        # print(imgurls)
-        class AppURLopener(urllib.request.FancyURLopener):
-            version = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.69 Safari/537.36"
-        urllib._urlopener = AppURLopener()
-        urllib._urlopener.retrieve(imgurls, str(page) + ".jpg")
+    print("Chapter", ch, ": is stored at ", os.getcwd())
+    for i in tqdm(range(1, fin + 1), desc="Chapter " + str(ch), ascii = True):
+        if all([os.path.isfile(str(page) + ".jpg") == False]):
+            url = baseAdd + str(ch) + "/" + str(page)
+            r = requests.get(url)
+            r.content
+            soup = BeautifulSoup(r.content);
+            imgurl = soup.find_all("img", {"id":"img"})
+            imgurls = str(imgurl)
+            start = imgurls.find('src=\"') + 5
+            end = imgurls.find('.jpg') + 4
+            imgurls = imgurls[start:end]
+            # print(imgurls)
+            class AppURLopener(urllib.request.FancyURLopener):
+                version = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.69 Safari/537.36"
+            urllib._urlopener = AppURLopener()
+            urllib._urlopener.retrieve(imgurls, str(page) + ".jpg")
         page = page + 1
     os.chdir('..')
     page = 1
